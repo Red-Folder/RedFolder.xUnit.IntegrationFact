5e501471f8dd7daa95e68fc57957934348a7556e
Red-Folder <markbryantaylor@gmail.com>
2017-05-15 16:43:30 +0100

    Initial code version

---
+++ src/RedFolder.xUnit.IntegrationFact/RedFolder.xUnit.IntegrationFact/IntegrationFact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace RedFolder.xUnit.IntegrationFact
+{
+    public class IntegrationFactDiscoverer : IXunitTestCaseDiscoverer
+    {
+        readonly IMessageSink diagnosticMessageSink;
+
+        public IntegrationFactDiscoverer(IMessageSink diagnosticMessageSink)
+        {
+            this.diagnosticMessageSink = diagnosticMessageSink;
+        }
+
+        public IEnumerable<IXunitTestCase> Discover(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IAttributeInfo factAttribute)
+        {
+            return Enabled
+                ? new[] { new XunitTestCase(diagnosticMessageSink, discoveryOptions.MethodDisplayOrDefault(), testMethod) }
+                : Enumerable.Empty<IXunitTestCase>();
+        }
+
+        private bool Enabled
+        {
+            get
+            {
+                var enabled = Environment.GetEnvironmentVariable("XUNIT_INTERGRATION_FACT_ENABLED");
+                return (enabled.ToLower() == "true");
+            }
+        }
+    }
+
+    [XunitTestCaseDiscoverer("RedFolder.xUnit.IntegrationFact.IntegrationFactDiscoverer", "RedFolder.xUnit.IntegrationFact")]
+    public class IntegrationFactAttribute : FactAttribute
+    {
+    }
+}
